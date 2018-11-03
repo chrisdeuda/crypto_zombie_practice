7dadc48ce9bd2461700db541f1ec0f30811cadbc
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-03 21:42:13 +0800

    Add feedOnKitty in ZombieFeeding

--- ethereum/contracts/ZombieFeeding.sol
+++ ethereum/contracts/ZombieFeeding.sol
@@ -23,7 +23,7 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory {
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     // Initialize kittyContract here using `ckAddress` from above
-    KittyInterface KittyContract = KittyInterface(ckAddress);
+    KittyInterface kittyContract = KittyInterface(ckAddress);
 
     function feedAndMultiply (uint _zombieId, uint _targetDna) public{
         require( msg.sender == zombieToOwner[_zombieId]);
@@ -33,6 +33,12 @@ contract ZombieFeeding is ZombieFactory {
         uint newDna = (myZombie.dna + _targetDna ) /2;
 
         _createZombie("NoName",newDna);
+    }
+
+    function feedOnKitty( uint _zombieId, uint _kittyId) public {
+        uint kittyDna ;
+        (,,,,,,,,,kittyDna ) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId,kittyDna );
     }
 
 }
