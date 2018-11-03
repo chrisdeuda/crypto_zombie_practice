82ea84560e1ba4572ed07f731c9b8442639428e8
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-03 21:18:21 +0800

    Add creation of the new dna

--- ethereum/contracts/ZombieFeeding.sol
+++ ethereum/contracts/ZombieFeeding.sol
@@ -6,10 +6,14 @@ import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
-  function feedAndMultiply (uint _zombieId, uint _targetDna) public{
-      require( msg.sender == zombieToOwner[_zombieId]);
+    function feedAndMultiply (uint _zombieId, uint _targetDna) public{
+        require( msg.sender == zombieToOwner[_zombieId]);
 
-      Zombie storage myZombie = zombies[_zombieId];
-  }
+        Zombie storage myZombie = zombies[_zombieId];
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna ) /2;
+
+        _createZombie("NoName",newDna);
+    }
 
 }
