7ba52d16514ae0dd133f8b916f946320d80c10f0
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-03 21:11:49 +0800

    Add feedAndMulitply function to zombie

--- ethereum/contracts/ZombieFeeding.sol
+++ ethereum/contracts/ZombieFeeding.sol
@@ -6,4 +6,10 @@ import "./ZombieFactory.sol";
 
 contract ZombieFeeding is ZombieFactory {
 
+  function feedAndMultiply (uint _zombieId, uint _targetDna) public{
+      require( msg.sender == zombieToOwner[_zombieId]);
+
+      Zombie storage myZombie = zombies[_zombieId];
+  }
+
 }
