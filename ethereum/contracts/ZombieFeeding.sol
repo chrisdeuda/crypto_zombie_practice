8209f02e83ff16f797a3e619b1267056f9553cd1
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-05 21:25:31 +0800

    Refactor the referrence of the crytokitties

--- ethereum/contracts/ZombieFeeding.sol
+++ ethereum/contracts/ZombieFeeding.sol
@@ -24,6 +24,10 @@ contract ZombieFeeding is ZombieFactory {
     address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     // Initialize kittyContract here using `ckAddress` from above
     KittyInterface kittyContract;
+    
+    function setKittyContractAddress(address _address) external{
+      kittyContract = KittyInterface(_address);
+    }
 
     function feedAndMultiply (uint _zombieId, uint _targetDna) public{
         require( msg.sender == zombieToOwner[_zombieId]);
