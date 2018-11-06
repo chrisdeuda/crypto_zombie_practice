69845b8ed9475da2517107001f7038d0d9535ea2
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-06 15:27:17 +0800

    Add random generantor in attacks

--- ethereum/contracts/ZombieAttack.sol
+++ ethereum/contracts/ZombieAttack.sol
@@ -4,4 +4,11 @@ import "./ZombieHelper.sol";
 
 contract ZombieBattle is ZombieHelper{
 
+    uint randNonce = 0;
+  
+    function randMod( uint _modulus ) internal returns (uint) {
+        randNonce++;
+        return (uint(keccak256(now, msg.sender, randNonce)) % _modulus);
+    }
+
 }
