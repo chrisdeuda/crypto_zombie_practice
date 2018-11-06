3928f6eefa8dd67342cdbc0ac0dbb4335c76b784
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-06 16:58:39 +0800

    Impements SafeMath in ZombieFactory

--- ethereum/contracts/ZombieFactory.sol
+++ ethereum/contracts/ZombieFactory.sol
@@ -1,8 +1,10 @@
 pragma solidity ^0.4.19;
 
 import "./ownable.sol";
+import "./SafeMath.sol";
 
 contract ZombieFactory is Ownable {
+    using SafeMath for uint256;
 
     event NewZombie(uint zombieId, string name, uint dna);
 
