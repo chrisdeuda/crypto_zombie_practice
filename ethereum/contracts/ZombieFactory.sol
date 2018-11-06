9a12a0a6d8a0f3ecea0dede69cceb45960f36762
Christopher Deuda <christopherdeuda@gmail.com>
2018-11-06 17:05:56 +0800

    Refactor use of the increments

--- ethereum/contracts/ZombieFactory.sol
+++ ethereum/contracts/ZombieFactory.sol
@@ -5,6 +5,8 @@ import "./SafeMath.sol";
 
 contract ZombieFactory is Ownable {
     using SafeMath for uint256;
+    using SafeMath32 for uint32;
+    using SafeMath16 for uint16;
 
     event NewZombie(uint zombieId, string name, uint dna);
 
@@ -37,7 +39,7 @@ contract ZombieFactory is Ownable {
         uint id = zombies.push(Zombie(_name, _dna, 1, uint32(now + cooldownTime), 0, 0 )) - 1;
         
         zombieToOwner[id] = msg.sender;
-        ownerZombieCount[msg.sender]++;
+        ownerZombieCount[msg.sender] =  ownerZombieCount[msg.sender].add(1);
 
         NewZombie(id, _name, _dna);
     } 
